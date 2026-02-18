e2a2c5066aed29d84448f10b14f1775078592d66
Alexpulse <alex.boulanger@pulselife.com>
2026-02-18 12:01:50 +0100

    feat: init

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export function Home() {
+  return (
+    <div className="min-h-screen bg-linear-to-b from-slate-900 via-slate-800 to-slate-900">
+      <Button>Home</Button>
+    </div>
+  )
+}
